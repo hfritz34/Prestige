7393aa2fa7f766cea23ea798aa61f25e0d38cca2
Henry Fritz <133249998+hfritz34@users.noreply.github.com>
2025-09-08 17:29:04 -0400

    bump max to 100 items on homepage

--- Prestige.Api/Endpoints/Profile/ProfileServices.cs
+++ Prestige.Api/Endpoints/Profile/ProfileServices.cs
@@ -24,7 +24,7 @@ namespace Prestige.Api.Endpoints.Profile
         {
         }
 
-        const int MAX_DISPLAYED = 60;
+        const int MAX_DISPLAYED = 100;
 
 
         public async Task<IEnumerable<UserTrackResponse>> GetTopTracksAsync(string userId)
